4218d06f129e97095eb07a20a75523add47a1ebd
gdvsainathreddy <gdvsainathreddy@gmail.com>
2024-03-21 17:20:25 +0530

    Update main.py
    working code on file upload,
    replace file is not working yet

--- w5100-pico-projects/main.py
+++ w5100-pico-projects/main.py
@@ -1,108 +1,100 @@
-from usocket import socket
-from machine import Pin,SPI
-import network
+import usocket as socket
+import uos
 import time
-import json
+import network
 
+HTML_TEMPLATE = """<!DOCTYPE html>
+<html>
+<head><title>File Server</title></head>
+<body>
+<h2>Files:</h2>
+<ul>{}</ul>
+<form method="POST" action="/upload" enctype="multipart/form-data">
+    <input type="file" name="file">
+    <input type="submit" value="Upload">
+</form>
+</body>
+</html>
+"""
 
-led = Pin(25, Pin.OUT)
-
-#W5x00 chip init
-def w5x00_init():
-    spi=SPI(0,2_000_000, mosi=Pin(19),miso=Pin(16),sck=Pin(18))
-    nic = network.WIZNET5K(spi,Pin(17),Pin(20)) #spi,cs,reset pin
-    nic.active(True)
-    
-    #None DHCP
-    nic.ifconfig(('192.168.0.22','255.255.255.0','192.168.0.1','8.8.8.8'))
-    
-    #DHCP
-    #nic.ifconfig('dhcp')
-    print('IP address :', nic.ifconfig())
-    
-    while not nic.isconnected():
-        time.sleep(1)
-        print(nic.regs())
-
-fan_status = {}
-gpio_status = {}
-lock_status = {}
-rgb_status = {}
-
-def set_response(client, json_type, response):
-    client.send('HTTP/1.0 200 OK\r\n')
-    if json_type:
-        client.send('Content-type: application/json\r\n\r\n')
-        client.sendall(response)
-    else:
-        client.send('\r\n')
-        client.sendall(response)
-
-def parse_path(path):
-    return path.split('/')[1:]
-
-def handle_request(client):
+def file_exists(filename):
     try:
-        request = client.recv(1024).decode('utf-8')
-        path = parse_path(request.split(' ')[1])
-        print(path)
-        if path[0] == 'security' and len(path) > 2:
-            response = "OK"
-            set_response(client, json_type=False, response=response)
-            return
-        elif path[0].startswith("gpio") and len(path) > 2:
-            pin = int(path[0][4:])
-            print(pin)
-            action = path[1]
-            if action in ['on', 'off', 'status']:
-                if action == 'on':
-                    gpio_status[pin] = 1
-                    response = "OK"
-                    led.value(1)
-                    # Add your GPIO handling code here
-                elif action == 'off':
-                    gpio_status[pin] = 0
-                    response = "OK"
-                    led.value(0)
-                    # Add your GPIO handling code here
-                elif action == 'status':
-                    if pin not in gpio_status:
-                        gpio_status[pin] = 0
-                    response = str(gpio_status[pin])
-                set_response(client, json_type=False, response=response)
-                return
-        # Add similar handling for other routes
-        
-        # If the URL doesn't match our pattern, serve a 404 response
-        response = {'status': 'error', 'message': 'Error from RPI'}
-        client.send('HTTP/1.0 404 Not Found\r\n')
-        client.send('Content-type: application/json\r\n\r\n')
-        client.sendall(json.dumps(response).encode())
-    except Exception as e:
-        print('Error:', e)
+        uos.stat(filename)
+        return True
+    except OSError:
+        return False
 
+def get_mac_address():
+    # Initialize the network interface
+    spi = network.SPI(1)
+    cs = machine.Pin(10)
+    rst = machine.Pin(9)
+    nic = network.WIZNET5K(spi, cs, rst, wiz_type=network.WIZNET5K.W5100S)
+    return nic.mac_address
 
-def main():
-    w5x00_init()
-    s = socket()
-    import time
+def serve_files(conn, addr):
+    request = conn.recv(1024).decode()
+    request_method = request.split(' ')[0]
+    if request_method == 'GET':
+        if request.split()[1] == '/files':
+            file_list = ''
+            for file in uos.listdir():
+                file_list += f'<li><a href="/files/{file}">{file}</a></li>'
+            response = HTML_TEMPLATE.format(file_list)
+        else:
+            response = 'HTTP/1.1 404 Not Found\r\n\r\n'
+    elif request_method == 'POST':
+        if '/upload' in request:
+            content_length = int(request.split('Content-Length: ')[1].split('\r\n')[0])
+            content = conn.recv(content_length)
+            filename = content.split(b'filename="')[1].split(b'"')[0].decode()
+            print("Filename: ",filename)
+            if file_exists(filename):
+                print("file exists")
+                response = "File already exists. Overwrite? (Yes/No)"
+                conn.sendall(response.encode())
+                confirmation = conn.recv(1024).decode().strip().lower()
+                if confirmation != 'yes':
+                    return
+            with open(filename, 'wb') as f:
+                f.write(content)
+            response = f"File {filename} uploaded successfully."
+        else:
+            response = 'HTTP/1.1 404 Not Found\r\n\r\n'
+    else:
+        response = 'HTTP/1.1 405 Method Not Allowed\r\n\r\n'
 
+    conn.sendall(response.encode())
+    conn.close()
+
+def start_server():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     while True:
         try:
-            s.bind(('', 80))
+            s.bind(("192.168.0.22", 80))
             break
         except OSError as e:
             if e.args[0] == 98:
                 print("Port 80 is in use, waiting...")
-                time.sleep(5)
+                print("5")
+                time.sleep(1)
+                print("4")
+                time.sleep(1)
+                print("3")
+                time.sleep(1)
+                print("2")
+                time.sleep(1)
+                print("1")
+                time.sleep(1)
             else:
                 raise
     s.listen(5)
+    print("Server started at 192.168.0.22:80")
+    #print("MAC Address:", get_mac_address())
 
     while True:
         conn, addr = s.accept()
-        handle_request(conn)
-        conn.close()
+        print("Connection from:", addr)
+        serve_files(conn, addr)
 
-if __name__ == "__main__":
-    main()
+start_server()
